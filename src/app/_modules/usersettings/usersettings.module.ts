c45c29a791ec98c016e9aa79bdf6344505781878
Kevin Weidner <weidnerk@gmail.com>
2020-04-30 19:26:03 -0400

    Put policy details on matcards.

--- src/app/_modules/usersettings/usersettings.module.ts
+++ src/app/_modules/usersettings/usersettings.module.ts
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { AppOverlayModule } from '../../overlay/overlay.module';
 import { ProgressSpinnerModule } from '../../progress-spinner/progress-spinner.module';
 import { UsersettingsRoutingModule } from './usersettings-routes.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [UsersettingsComponent],
@@ -29,7 +30,8 @@ import { UsersettingsRoutingModule } from './usersettings-routes.module';
     MatCheckboxModule,
     AppOverlayModule,
     ProgressSpinnerModule,
-    UsersettingsRoutingModule
+    UsersettingsRoutingModule,
+    MatCardModule
   ]
 })
 export class UsersettingsModule { }
