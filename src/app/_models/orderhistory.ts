c96d4c35bd3cf12506064ec6c15d663d7a60500d
Kevin Weidner <weidnerk@gmail.com>
2020-06-22 13:45:36 -0400

    Add repricerLastRan to dashboard.

--- src/app/_models/orderhistory.ts
+++ src/app/_models/orderhistory.ts
@@ -154,6 +154,7 @@ export class Dashboard {
     listed: number;
     oos: number;
     notListed: number;
+    repricerLastRan: Date;
 }
 export class StoreAnalysis {
     dbIsMissingItems: string[];
